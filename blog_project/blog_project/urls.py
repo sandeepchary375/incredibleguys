6cb6b8c8834183af52e144102b789219fb785a3f
sandeep chary <sndpchary375@gmail.com>
2019-08-31 21:48:44 -0700

    fifth commit

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.post_list_view),
+    url(r'^$', views.post_list_view,name="home"),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list_view, name='post_list_by_tag_name'),
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<post>[-\w]+)/$', views.post_detail_view,
         name='post_detail'),
